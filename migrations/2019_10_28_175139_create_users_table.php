ad1d89c1a4650d9de59739c9e1fc2c8fb8718f34
jonathan-martz <github@jmartz.de>
2019-12-27 18:10:11 +0100

    add migration for users, add ModelUserProvider.php, update composer.json

---
+++ migrations/2019_10_28_175139_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('username_hash');
+            $table->string('password');
+            $table->string('firstname');
+            $table->string('secondname');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('active')->default(false);
+            $table->integer('RID')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
