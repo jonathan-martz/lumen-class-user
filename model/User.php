513a0bd724661efe9cf2e13bf692a7b23e764568
jonathan-martz <github@jmartz.de>
2019-12-31 21:08:29 +0100

    add logic for getRoleName and getLastLogin

--- model/User.php
+++ model/User.php
@@ -6,6 +6,7 @@ use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Laravel\Lumen\Auth\Authorizable;
 
 /**
@@ -33,6 +34,35 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $hidden = [
         'password',
     ];
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+    }
+
+    public function getLastLogin()
+    {
+        $loginTry = DB::table('login_try')
+            ->where('username_hash', '=', sha1($this->getAttribute('username')))
+            ->orderByDesc('created_at');
+
+        if($loginTry->count() > 0) {
+            $loginData = $loginTry->first();
+            return date('d.m.Y H:i:s', $loginData->created_at);
+        }
+    }
+
+    public function getRoleName()
+    {
+        $user = DB::table('user_role')->where('id', '=', $this->getAttribute('RID'));
+
+        if($user->count() === 1) {
+            $userData = $user->first();
+            return $userData->name;
+        }
+
+        return 'user';
+    }
 
     /**
      * @return bool
