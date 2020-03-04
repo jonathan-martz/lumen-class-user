0f8fb3344f61bdb35283c2e0306788f0a4b22bd9
jonathan-martz <github@jmartz.de>
2020-03-04 16:58:47 +0100

    remove useless stuff

--- model/User.php
+++ model/User.php
@@ -60,6 +60,8 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
             $loginData = $loginTry->first();
             return date('d.m.Y H:i:s', $loginData->created_at);
         }
+
+        return false;
     }
 
     /**
@@ -74,15 +76,6 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
             return $userData->name;
         }
 
-        return 'user';
+        return 'guest';
     }
-
-    /**
-     * @return bool
-     */
-    public function getActive()
-    {
-        return (bool)$this->getAttribute('active');
-    }
-
 }
