4369b446b721b8e0b6200c13c26395fc2e6c32f0
jonathan-martz <github@jmartz.de>
2020-03-04 15:03:25 +0100

    remove lumen class

--- model/User.php
+++ model/User.php
@@ -7,7 +7,6 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-use Laravel\Lumen\Auth\Authorizable;
 
 /**
  * Class User
@@ -15,7 +14,7 @@ use Laravel\Lumen\Auth\Authorizable;
  */
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
-    use Authenticatable, Authorizable;
+    use Authenticatable;
 
     /**
      * The attributes that are mass assignable.
